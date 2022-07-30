e8d608e6a80eacd58e962117152954f700b957d0
hosamgmail <hosamgharabli@gmail.com>
2022-07-30 18:58:23 +0300

    fix issue

--- resources/views/layout/partials/head.blade.php
+++ resources/views/layout/partials/head.blade.php
@@ -1,7 +1,8 @@
 <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/favicon.png">
-<link href='{{asset('css/style.min.css')}}' rel="stylesheet">
-<link href="{{asset('css/jquery-ui.min.css')}}" rel="stylesheet">
 <link href="{{asset('css/chartist.min.css')}}" rel="stylesheet">
 <link href="{{asset('css/c3.min.css')}}" rel="stylesheet">
+<link href='{{asset('css/style.min.css')}}' rel="stylesheet">
+<link href="{{asset('css/jquery-ui.min.css')}}" rel="stylesheet">
+
 <link href='{{asset('css/main.css')}}' rel="stylesheet">
 <script src="{{ asset('js/jquery.min.js') }}"></script>
